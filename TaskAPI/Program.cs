172cb05dbc9ba9d1460cbf1b3fa3edb1c0eb7c4d
Beesan-demaide <beesan.demaide@gmail.com>
2026-09-13 14:47:00 +0300

    Add the missing GET /tasks/{id}

--- TaskAPI/Program.cs
+++ TaskAPI/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 using TaskAPI;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,29 +18,21 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
 app.MapGet("/tasks", (TaskDbContext db) =>
     {
         return db.Tasks.ToList();
+    });
+
+app.MapGet("/tasks/{id}",(int id,TaskDbContext db) =>
+    {
+       var task = db.Tasks.FirstOrDefault(t => t.Id == id);
+
+        if(task is null)
+        {
+            return Results.NotFound();
+        }
+        return Results.Ok(task);
     });
 app.MapPost("/tasks", (CreateRequest request, TaskDbContext db) =>
 {
@@ -49,7 +42,7 @@ app.MapPost("/tasks", (CreateRequest request, TaskDbContext db) =>
     };
     db.Tasks.Add(task);
     db.SaveChanges();
-    return task;
+    return Results.Created($"/tasks/{task.Id}", task);
 });
 
 app.Run();
